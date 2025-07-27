aeff8ef66a4e202537853b8aec0fea9f2534891f
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-27 15:00:34 -0300

    Update Sendwebhook.lua

--- Sendwebhook.lua
+++ Sendwebhook.lua
@@ -1,26 +1,21 @@
 local HttpService = game:GetService("HttpService")
 
 function enviarWebhook(webhook, title, itens)
-    local hora = os.date("%H:%M")
+    local agora = os.time()
+    local minutosArredondados = agora - (os.date("*t", agora).min % 5) * 60
+    local timestampDiscord = "<t:" .. minutosArredondados .. ":t>"
+
+    local mensagem = table.concat(itens, "\n")
     local data = {
         content = "",
         embeds = {{
-            title = title .. " - " .. hora,
+            title = title .. " - " .. timestampDiscord,
             color = 65280,
-            fields = {}
+            description = mensagem
         }}
     }
 
-    for _, texto in ipairs(itens) do
-        table.insert(data.embeds[1].fields, {
-            name = texto,
-            value = "",
-            inline = false
-        })
-    end
-
     local jsonData = HttpService:JSONEncode(data)
-
     local req = syn and syn.request or http and http.request or request or nil
     if req then
         req({
