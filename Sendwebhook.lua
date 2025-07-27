d05c3e7bcea2386624f168362aaea7e095edcdba
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-07-27 13:59:41 -0300

    Update Sendwebhook.lua

--- Sendwebhook.lua
+++ Sendwebhook.lua
@@ -1,1 +1,26 @@
+local HttpService = game:GetService("HttpService")
+local player = game:GetService("Players").LocalPlayer
 
+function enviarWebhook(webhook, title, itens)
+    local data = {
+        ["content"] = "",
+        ["embeds"] = {{
+            ["title"] = title,
+            ["color"] = 65280,
+            ["fields"] = {},
+            ["footer"] = {
+                ["text"] = "Player: " .. player.Name .. " | " .. os.date("%d/%m/%Y %H:%M:%S")
+            }
+        }}
+    }
+
+    for _, texto in ipairs(itens) do
+        table.insert(data.embeds[1].fields, {
+            ["name"] = "Item",
+            ["value"] = texto,
+            ["inline"] = false
+        })
+    end
+
+    HttpService:PostAsync(webhook, HttpService:JSONEncode(data))
+end
